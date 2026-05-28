c348eaab71dcf602476b5922e395884a7b2d9f0f
Pranjal Chavan <pracha041105@gmail.com>
2026-05-28 17:10:54 +0530

    add 03-router-contact-server (continuation of 01-mw-contact-server with routers, controllers, html files, and body parsing)

---
+++ express-practice/03-router-contact-server/app.js
@@ -0,0 +1,26 @@
+// Core Module
+const path = require('path');
+
+// External Module
+const express = require('express');
+
+//Local Module
+const homeRouter = require('./routes/homeRouter');
+const contactRouter = require('./routes/contactRouter');
+const rootDir = require('./utils/pathUtils');
+
+const app = express();
+
+app.use(express.urlencoded());
+
+app.use(homeRouter);
+app.use(contactRouter);
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "404.html"));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on address http://localhost:${PORT}`);
+});
