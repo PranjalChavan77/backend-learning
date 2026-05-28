d7db5d8b7c92a21493a731c61e173ea616f99196
Pranjal Chavan <pracha041105@gmail.com>
2026-05-28 14:42:46 +0530

    Added Express middleware and contact form practice

---
+++ express-practice/01-mw-contact-server/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("First Middleware", req.url, req.method);
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("Second Middleware", req.url, req.method);
+    next();
+});
+
+// app.use((req, res, next) => {
+//     console.log("Third Middleware", req.url, req.method);
+//     res.send(
+//         "<h1>Welcome to Page</h1>"
+//     );
+//     next();
+// });
+
+app.get("/", (req, res, next) => {
+    console.log("Handling / for GET", req.url, req.method);
+    res.send("<h1>Welcome to my Page !</h1>");
+});
+
+app.get("/contact-us", (req, res, next) => {
+    console.log("Handling /contact-us for GET", req.url, req.method);
+    res.send(
+        `<h1>Please give your details here: </h1>
+        <form action="/contact-us" method="POST">
+         <input type="text" name="name" placeholder="Enter your Name" />
+         <input type="text" email="email" placeholder="Enter your Email" />
+         <input type="Submit" />
+        </form>`
+    );
+});
+
+app.post("/contact-us", (req, res, next) => {
+    console.log("Handling /contact-us for POST", req.url, req.method);
+    res.send("<h1>We will contact you shortly.</h1>");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on address http://localhost:${PORT}`);
+});
